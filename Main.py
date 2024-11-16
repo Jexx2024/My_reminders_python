445d95228e2273b9141218b6982f3522482554d0
Jexx2024 <lyubimov.evg@gmail.com>
2024-11-16 19:06:51 +0300

    Импортированы нужные библиотеки, создано главное окно.

---
+++ Main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from tkinter import messagebox as mb
+from tkinter import simpledialog as sd
+import datetime
+import time
+import pygame
+
+window=Tk()
+window.title("Напоминание со звуком")
+window.mainloop()
