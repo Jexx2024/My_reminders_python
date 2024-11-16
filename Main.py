3413b047962d9b492f92b379d8d8882a13cae729
Jexx2024 <lyubimov.evg@gmail.com>
2024-11-16 19:16:31 +0300

    Добавлен интерфейс(метка и кнопка)

--- Main.py
+++ Main.py
@@ -5,6 +5,16 @@ import datetime
 import time
 import pygame
 
+def sett():
+    pass
 window=Tk()
 window.title("Напоминание со звуком")
+
+label=Label(text="Установите нааоминание", font=('Courier', 20))
+label.pack(pady=20)
+
+button=Button(text="Установить", font=('Courier', 20), command=sett)
+button.pack(pady=20)
+
+
 window.mainloop()
